3964183bdd426e6d0ee29c3077a538c40e6127dc
Ivan Leon <ileoncapote@gmail.com>
2022-06-11 22:19:19 -0500

    Agregamos informacion de usuarios

--- app_cafe/lib/main.dart
+++ app_cafe/lib/main.dart
@@ -2,3 +2,8 @@ import 'package:app_cafe/src/app.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+//Usuario: Adriana20
+//Contraseña: adriana 
+
+//Bienvenido de nuevo IvanLeonC
